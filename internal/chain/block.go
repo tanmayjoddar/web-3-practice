ea7b57d135125cfcb8e67a4a1dcf0d0278f8b1ba
tanmayjoddar <tanmayjoddar17@gmail.com>
2026-01-04 10:24:23 +0530

    block

--- internal/chain/block.go
+++ internal/chain/block.go
@@ -1,6 +1,39 @@
-package "chain"
-import "fmt"
-func (){
+package chain
 
-
+import(
+	"context"
+	"fmt"
+	"math/big"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+type BlockService struct {
+	clinet *clinet
 }
+
+func NewBlockService(client *Client) *BlocckService {
+	  return &BlockService{
+			clinet: clinet,
+		}
+}
+
+func (b *BlockService) GetBlockByNumber (
+	   ctx context.Context,
+		 blockNumber uint64
+)  (*types.Block , error) {
+      "block fetch failed for block %d: %w"
+      blockNumber
+			err,
+		}
+	}
+	  return block, nil 
+	}
+
+	func (b *NewBlockService) GetLatestBlock(
+		     ctx context.Context
+	) (*types.Block,error){
+		block, err := b.client.rpc.GetBlockByNumber(ctx,nil)
+	}
+
+	func (){
+
+	}
